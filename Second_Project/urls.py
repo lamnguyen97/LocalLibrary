b74d2ee4d1bf9762be7c3698cdc69d5cdf571dfa
lamnguyen97 <1611743@hcmut.edu.vn>
2018-12-14 11:22:30 +0700

    Views, Template, URL for Book,Author list and detail

--- Second_Project/urls.py
+++ Second_Project/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/catalog/', include('locallibrary.urls')),
-    path('',RedirectView.as_view(url='/catalog/', permanent=True))
+    path('catalog/', include('locallibrary.urls')),
+    path('',RedirectView.as_view(url='catalog/', permanent=True))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- locallibrary/views.py
+++ locallibrary/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book,BookInstance,Author,Genre
 
+def index(request):
+    template_name = 'index.html'
+    num_book = Book.objects.count()
+    num_copy = BookInstance.objects.count()
+    num_author = Author.objects.count()
+    num_avail = BookInstance.objects.filter(status='a').count()
+    num_genre = Genre.objects.count()
+    return render(request,'index.html', {'num_book':num_book,'num_copy':num_copy,'num_author':num_author,'num_avail':num_avail,
+                                         'num_genre': num_genre})
 # Create your views here.
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    content_object_name ="book"
+    template_name ='book_detail.html'
+
+class AuthorListView(generic.ListView):
+    model = Author
+    context_object_name = 'author_list'
+    template_name ='author_list.html'
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    context_object_name = 'author'
+    template_name ='author_detail.html'
